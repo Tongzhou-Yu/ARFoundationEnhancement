7ffb6db0650d6c355deb5ef59422a5d719071d9a
Tongzhou-Yu <tongzhou.yu@network.rca.ac.uk>
2020-08-26 16:47:49 +0800

    ARFoundation 4.0 Required

---
+++ ar-civichotel-arfoundation/Assets/Scripts/TouchManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchManager : MonoBehaviour
+{
+    public Material onLight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount >0)
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit))
+            {
+                var rig = hit.collider.GetComponent<Rigidbody>();
+                if(rig !=null)
+                {
+                    rig.GetComponent<MeshRenderer>().material = onLight;
+                    rig.transform.GetChild(0).gameObject.SetActive(true);
+                }
+            }
+        }
+        
+    }
+}
